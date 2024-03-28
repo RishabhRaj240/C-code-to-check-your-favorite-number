9cc37d6a14aaa5e50c817c80377c95957ec3ae0d
Rishabh Raj <rishabhraj240405@gmail.com>
2024-03-28 18:40:15 +0530

    Add files via upload

---
+++ secondclass.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int myFavoriteNumber =24;
+
+	printf("Please Enter Your Favorite Number\n");
+	scanf_s("%d", &myFavoriteNumber);
+	printf("Your entered number: %d\n", myFavoriteNumber);
+
+	return 0;
+}
